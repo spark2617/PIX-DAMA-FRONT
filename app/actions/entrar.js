272e13d894bed5d929956024f980bc3612e1beba
jhonatan <jhonatandesouzade13@gmail.com>
2025-04-03 16:32:55 -0400

    ajuste no controller login 3

--- app/actions/entrar.js
+++ app/actions/entrar.js
@@ -1,36 +1,36 @@
-'use client'
+// 'use client'
 
 
-export async function entrar(email, senha) {
-    const supabase = createClient()
+// export async function entrar(email, senha) {
+//     const supabase = createClient()
     
-    let sucessoLogin = false
-    let resposta = {}
+//     let sucessoLogin = false
+//     let resposta = {}
     
-    const data = {
-      email: email,
-      password: senha
-    }
+//     const data = {
+//       email: email,
+//       password: senha
+//     }
 
   
-    const { error } = await supabase.auth.signInWithPassword(data)
+//     const { error } = await supabase.auth.signInWithPassword(data)
   
-    if (error) {
-      sucessoLogin = false 
-      console.log(error)
-      resposta = {
-        sucessoLogin,
-        mensagem: "Email ou senha Incorretos"
-      }
-    } else {
-      sucessoLogin = true 
+//     if (error) {
+//       sucessoLogin = false 
+//       console.log(error)
+//       resposta = {
+//         sucessoLogin,
+//         mensagem: "Email ou senha Incorretos"
+//       }
+//     } else {
+//       sucessoLogin = true 
 
-      resposta = {
-        sucessoLogin,
-        mensagem: 'Login Realizado com sucesso'
-      }
-    }
+//       resposta = {
+//         sucessoLogin,
+//         mensagem: 'Login Realizado com sucesso'
+//       }
+//     }
 
-    return resposta
+//     return resposta
     
-}
+// }
